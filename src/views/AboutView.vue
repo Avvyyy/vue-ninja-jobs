358bc8f53787ef5937259f3ce9449513487b59ac
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-12-07 03:00:59 +0100

    First Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum fugiat molestiae reiciendis quo incidunt perferendis consequatur ad, corrupti, pariatur alias nesciunt sit expedita. Accusamus ex, perferendis quaerat tenetur debitis architecto sint dolore et maiores nemo omnis eligendi voluptates, asperiores temporibus?</p>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum fugiat molestiae reiciendis quo incidunt perferendis consequatur ad, corrupti, pariatur alias nesciunt sit expedita. Accusamus ex, perferendis quaerat tenetur debitis architecto sint dolore et maiores nemo omnis eligendi voluptates, asperiores temporibus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum fugiat molestiae reiciendis quo incidunt perferendis consequatur ad, corrupti, pariatur alias nesciunt sit expedita. Accusamus ex, perferendis quaerat tenetur debitis architecto sint dolore et maiores nemo omnis eligendi voluptates, asperiores temporibus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum fugiat molestiae reiciendis quo incidunt perferendis consequatur ad, corrupti, pariatur alias nesciunt sit expedita. Accusamus ex, perferendis quaerat tenetur debitis architecto sint dolore et maiores nemo omnis eligendi voluptates, asperiores temporibus?</p>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
